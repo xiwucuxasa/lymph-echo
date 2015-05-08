d37fdbff1e8a64d883b39472ce7ff7db14e99e41
mamachanko <max@rootswiseyouths.com>
2015-05-08 16:38:14 +0200

    fix tests

--- tests.py
+++ tests.py
@@ -1,7 +1,12 @@
 from lymph.testing import RPCServiceTestCase
 
+from echo import Echo
+
 
 class EchoTest(RPCServiceTestCase):
+
+    service_class = Echo
+    service_name = 'echo'
 
     def test_echo(self):
         self.assertEqual(
